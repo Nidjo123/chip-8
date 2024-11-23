a5c121f196931201a3b9a382e41e620833c9808c
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-11-23 16:38:17 +0100

    Handle sprite clipping

--- src/com/nidjo123/chip8/instructions/DrawInstr.java
+++ src/com/nidjo123/chip8/instructions/DrawInstr.java
@@ -10,13 +10,21 @@ public class DrawInstr extends Instruction {
     @Override
     public void execute(Chip8.Context context) {
         context.registers[0xF] = 0;
-        int startX = context.registers[registerX];
-        int startY = context.registers[registerY];
+        int width = context.display.getWidth();
+        int height = context.display.getHeight();
+        int startX = context.registers[registerX] % width;
+        int startY = context.registers[registerY] % height;
         for (int y = 0; y < immediate4; y++) {
+            if (y >= height) {
+                break;
+            }
             byte spriteByte = context.memory.getByte(context.indexRegister + y);
             for (int x = 7; x >= 0; x--) {
+                int xPos = startX + 7 - x;
+                if (xPos >= width) {
+                    continue;
+                }
                 if (((spriteByte >> x) & 0x1) != 0) {
-                    int xPos = startX + 7 - x;
                     int yPos = startY + y;
                     context.display.togglePixel(xPos, yPos);
                     if (!context.display.getPixel(xPos, yPos)) {
